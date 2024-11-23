e2ab40440d4e78ddccc6aeaeb1bd6cce0667d29d
0huey <34045167+0huey@users.noreply.github.com>
2024-11-22 20:22:45 -0700

    cpp day 1 - remove using namespace std

--- cpp/2023/day1/solve.cpp
+++ cpp/2023/day1/solve.cpp
@@ -1,35 +1,38 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
-using namespace std;
+const std::string digits = "0123456789";
 
-const string digits = "0123456789";
-
-int main(int argc, char* argv[]) {
-	if (argc < 2) {
+int main(int argc, char* argv[])
+{
+	if (argc < 2)
+	{
 		return 1;
 	}
 
-	ifstream chal_file (argv[1]);
+	std::ifstream chal_file (argv[1]);
 
-	if (!chal_file.is_open()) {
+	if (!chal_file.is_open())
+	{
 		return 2;
 	}
 
 	int result = 0;
 
-	string line;
+	std::string line;
 
-	while (getline (chal_file, line)) {
+	while (std::getline(chal_file, line))
+	{
 		char num[3];
 		num[0] = line[ line.find_first_of(digits, 0) ];
 		num[1] = line[ line.find_last_of(digits, line.npos) ];
 		num[2] = '\0';
 
-		result += stoi( string(num) );
+		result += std::stoi( std::string(num) );
 	}
 
-	cout << result << "\n";
+	std::cout << result << "\n";
 
 	return 0;
 }
