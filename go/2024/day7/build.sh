d23b3d8ca0663333fa1f3cfb013c51402d1ac568
0huey <0huey@example.com>
2025-09-20 22:09:56 -0500

    go 2024 day 7

---
+++ go/2024/day7/build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+go build -o part1 part1.go 
+go build -o part2 part2.go
---
+++ go/2024/day7/part1.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+type Operator int
+
+const (
+	Add Operator = iota
+	Mul
+	NumOperators
+)
+
+type Chal struct {
+	Answer int
+	Values []int
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("no file specified")
+		return
+	}
+
+	data, err := Parse(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+
+	var sum int
+	for _, c := range data {
+		ops := slices.Repeat([]Operator{Add}, len(c.Values)-1)
+		if Recurse(c, ops) {
+			sum += c.Answer
+		}
+	}
+	fmt.Println(sum)
+}
+
+func Recurse(data Chal, ops []Operator) bool {
+	sum := data.Values[0]
+
+	for i, o := range ops {
+		switch o {
+		case Add:
+			sum += data.Values[i+1]
+		case Mul:
+			sum *= data.Values[i+1]
+		}
+	}
+
+	if sum == data.Answer {
+		return true
+	}
+
+	for i := range slices.Backward(ops) {
+		ops[i]++
+		if ops[i] == NumOperators && i == 0 {
+			return false
+		} else if ops[i] == NumOperators {
+			ops[i] = 0
+		} else {
+			break
+		}
+	}
+	return Recurse(data, ops)
+}
+
+func Parse(filename string) ([]Chal, error) {
+	var parsed []Chal
+
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return parsed, err
+	}
+
+	data2 := strings.TrimSpace(string(data))
+
+	for line := range strings.Lines(data2) {
+		var c Chal
+
+		linesplit := strings.Split(line, ":")
+
+		c.Answer, err = strconv.Atoi(linesplit[0])
+		if err != nil {
+			return parsed, err
+		}
+
+		vals := strings.Split(strings.TrimSpace(linesplit[1]), " ")
+
+		for _, n := range vals {
+			x, err := strconv.Atoi(n)
+			if err != nil {
+				return parsed, err
+			}
+			c.Values = append(c.Values, x)
+		}
+		parsed = append(parsed, c)
+	}
+	return parsed, nil
+}
